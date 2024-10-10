177efd368c809922c8d336f5f14598b9ff794c25
Trevor-Acheff <trevacheff@gmail.com>
2024-10-10 19:56:55 +0000

    grammar fixes

--- Main.java
+++ Main.java
@@ -23,23 +23,30 @@ public class Main
 
   public static void main(String[] args)
   {
-    System.out.println( "Welcome to our project! This program will tell you how many dyas you have to complete an asiignment. You can put up to five assingnemts into the program.");
-    System.out.println( " first we will need todays date.");
+    System.out.println( "Welcome to our project! This program will tell you how many days you have to complete an assignment.\n You can put up to five assignments into the program.");
+    System.out.println( "First we will need today's date.");
 
 
     int monthToday, dayToday, yearToday;
     
     monthToday = UtilityBelt.readInt("What month is it today? (Ex. Jan = 1, July = 7)\n", 1, 12); 
     dayToday = UtilityBelt.readInt("What day is it?\n", 1, 31);
-    yearToday = UtilityBelt.readInt("What year?\n", 2020,2024);
+    yearToday = 2024;
+
+    int todayJulian= Main.calcJulianDate(monthToday,dayToday,yearToday);
     String todayDate = Main.date(monthToday, dayToday, yearToday);
-    int todayjulian= Main.calcJulianDate(monthToday,dayToday,yearToday);
     System.out.println("Today's date is " + todayDate);
 
-     String assignment1= Main.AssignName();
-     int assigndate1=Main.AssignDate();
-     int differnce1=Main.calcdifference(assigndate1,todayjulian);
-     System.out.println(" You have "+ differnce1 +" days left to complete the assignment"+assignment1);
+    String assignment1, assignment2, assignment3, assignment4, assignment5;
+    int assignDate1, assignDate2, assignDate3, assignDate4, assignDate5;
+    int difference1, difference2, difference3, difference4, difference5;
+
+
+    //Assignment 1
+    assignment1 = Main.assignName();
+    assignDate1 = Main.assignDate();
+    difference1 = Main.calcDifference(assignDate1,todayJulian);
+    System.out.println("You have "+ difference1 +" days left to complete the assignment "+assignment1);
 
   }
 
@@ -59,24 +66,27 @@ public class Main
     return JDN;
   }
 
-  public static String AssignName()
+  public static String assignName()
   {
-    String Name = UtilityBelt.readString("What is the name of the assignment?", 1, 99);
-    return Name;
+    String name = UtilityBelt.readString("What is the name of the assignment?", 1, 99);
+    return name;
   }
 
-  public static int AssignDate()
+  public static int assignDate()
   {
-    int month = UtilityBelt.readInt("What month is the assingment due?", 1, 12);
-    int day = UtilityBelt.readInt("What day is the assignment due?", 1, 31);
-    int year = UtilityBelt.readInt("What year is the assignment due?", 2024, 2025);
+    int month = UtilityBelt.readInt("What month is the assingment due?\n", 1, 12);
+    int day = UtilityBelt.readInt("What day is the assignment due?\n", 1, 31);
+    int year = UtilityBelt.readInt("What year is the assignment due?\n", 2024, 2025);
+
+    String date = Main.date(month,day,year);
+    System.out.println("This assignment is due on " + date);
 
     int juliandate = Main.calcJulianDate(month, day, year);
     return juliandate;
   }
-  public static int calcdifference(int date1,int date2)
+  public static int calcDifference(int date1,int date2)
   {
-    int difference= date1 - date2;
+    int difference = date1 - date2;
     return difference;
   }
 }
